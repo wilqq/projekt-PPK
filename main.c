23b4c99df238b98819fa7ba3cf354e6751b5576f
Mateusz Wilczyński <majko7@interia.pl>
2015-06-09 15:43:03 +0200

    refactor

--- main.c
+++ main.c
@@ -23,26 +23,22 @@ int main()
     FILE *input;
     input = fopen("we.txt", "r");
 
-    if(input != NULL)
-    {
-        parse_file(input, &head);
+    if(input != NULL) {
+        read_file(input, &head);
         fclose(input);
         save_list("wy.txt", head);
     }
     else
-    {
         printf("Nie udalo sie otworzyc pliku!\n");
-    }
 
     return 0;
 }
 
-void parse_file(FILE* plik, list_el** head)
+void read_file(FILE* plik, list_el** head)
 {
     char *token;
-    char line[LENGTH], author[LENGTH], title[LENGTH], labels[LENGTH], label[LENGTH] ;
-    while(!feof(plik))
-    {
+    char line[LENGTH], author[LENGTH], title[LENGTH], labels[LENGTH], label[LENGTH];
+    while(!feof(plik)) {
         fscanf(plik, "%[^;];", author);
         fscanf(plik, "%[^;];", title);
         fscanf(plik, "%[^\n];", labels);
@@ -59,47 +55,40 @@ void parse_file(FILE* plik, list_el** head)
 
 void add_to_list(list_el** head, char *word, char *author, char *title)
 {
-    list_el *tmp = *head;
-    if((*head) == NULL){
-        tmp = malloc (sizeof(list_el));
-        strcpy(tmp->label, word);
-        tmp->next = NULL;
-        tmp->under = NULL;
-        *head = tmp;
-        add_to_u_list(tmp, author, title);
+    list_el *wsk, *prev, *new_el;
+    if((*head) == NULL) {
+        new_el = malloc (sizeof(list_el));
+        strcpy(new_el->label, word);
+        new_el->next = NULL;
+        new_el->under = NULL;
+        *head = new_el;
     }
-    else
-    {
-        list_el *wsk, *prev, *new_el;
-        if(strcmp(tmp->label, word) > 0){
+    else {
+        if(strcmp((*head)->label, word) > 0) {
             new_el = malloc (sizeof(list_el));
             new_el->under = NULL;
             strcpy(new_el->label, word);
-            new_el->next = tmp;
+            new_el->next = *head;
             *head = new_el;
-            add_to_u_list(new_el, author, title);
         }
         else {
-
-            wsk = tmp;
-            while (wsk != NULL && strcmp(wsk->label, word) < 0)
-            {
+            wsk = *head;
+            while (wsk != NULL && strcmp(wsk->label, word) < 0) {
                 prev = wsk;
                 wsk = wsk->next;
             }
-            if(wsk == NULL || strcmp(wsk->label, word))
-            {
+            if(wsk == NULL || strcmp(wsk->label, word)) {
                 new_el = malloc (sizeof(list_el));
                 new_el->under = NULL;
                 strcpy(new_el->label, word);
                 new_el->next = wsk;
                 prev->next = new_el;
-                wsk = new_el;
             }
-
-            add_to_u_list(wsk, author, title);
+            else
+                new_el = wsk;
         }
     }
+    add_to_u_list(new_el, author, title);
     print_list(*head);
 }
 
@@ -110,74 +99,66 @@ void add_to_u_list(list_el* label_head, char *author, char *title)
     strcat(line, ";");
     strcat(line, title);
     u_list_el *wsk, *prev, *new_el;
+
+    new_el = malloc (sizeof(u_list_el));
+    strcpy(new_el->line, line);
     wsk = label_head->under;
+
     if(wsk == NULL) {
-        new_el = malloc (sizeof(u_list_el));
-        strcpy(new_el->line, line);
         new_el->next = NULL;
         label_head->under = new_el;
     } else if (strcmp(wsk->line, line) > 0) {
-        new_el = malloc (sizeof(u_list_el));
-        strcpy(new_el->line, line);
         new_el->next = wsk;
         label_head->under = new_el;
     } else {
-
-        while (wsk != NULL && strcmp(wsk->line, line) < 0)
-        {
+        while (wsk != NULL && strcmp(wsk->line, line) < 0) {
             prev = wsk;
             wsk = wsk->next;
         }
-        new_el = malloc (sizeof(u_list_el));
-        strcpy(new_el->line, line);
         new_el->next = wsk;
         prev->next = new_el;
     }
 }
 
- void save_list(char *file_name, list_el *head)
- {
+void save_list(char *file_name, list_el *head)
+{
     FILE *output;
     output = fopen(file_name, "w");
     list_el *wsk = head;
-    while( wsk != NULL )
-     {
-     fprintf(output, "%s\n", wsk->label);
-     save_u_list(output, wsk->under);
-     fprintf(output, "\n");
-     wsk = wsk->next;
-     }
- }
+    while( wsk != NULL ) {
+        fprintf(output, "%s\n", wsk->label);
+        save_u_list(output, wsk->under);
+        fprintf(output, "\n");
+        wsk = wsk->next;
+    }
+}
 
-  void save_u_list(FILE* output, u_list_el *head)
- {
-   u_list_el *wsk = head;
-   while( wsk != NULL )
-     {
-     fprintf(output, "%s\n", wsk->line);
-     wsk = wsk->next;
-     }
- }
+void save_u_list(FILE* output, u_list_el *u_head)
+{
+    u_list_el *wsk = u_head;
+    while( wsk != NULL ) {
+        fprintf(output, "%s\n", wsk->line);
+        wsk = wsk->next;
+    }
+}
 
 
- void print_list(list_el *head)
- {
+void print_list(list_el *head)
+{
     printf("\n");
-   list_el *wsk = head;
-   while( wsk != NULL )
-     {
-     printf ("-%s-\n", wsk->label);
-     print_u_list(wsk->under);
-     wsk = wsk->next;
-     }
- }
+    list_el *wsk = head;
+    while( wsk != NULL ) {
+        printf ("-%s-\n", wsk->label);
+        print_u_list(wsk->under);
+        wsk = wsk->next;
+    }
+}
 
-  void print_u_list(u_list_el *head)
- {
-   u_list_el *wsk = head;
-   while( wsk != NULL )
-     {
-     printf ("- -%s-\n", wsk->line);
-     wsk = wsk->next;
-     }
- }
+void print_u_list(u_list_el *u_head)
+{
+    u_list_el *wsk = u_head;
+    while( wsk != NULL ) {
+        printf ("- -%s-\n", wsk->line);
+        wsk = wsk->next;
+    }
+}
